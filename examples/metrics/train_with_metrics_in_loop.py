a2f980a561a29561eb1e644f96fba3802f31bd15
Chris Hughes <chris.hughes@microsoft.com>
2021-11-02 09:05:31 +0000

    Add examples

---
+++ examples/metrics/train_with_metrics_in_loop.py
@@ -0,0 +1,115 @@
+########################################################################
+# This is an accelerated example of the PyTorch "Transfer Learning for Computer Vision Tutorial"
+# written by Sasank Chilamkurthy, available here:
+# https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html,
+# which demonstrates fine-tuning a ResNet18 model to classify ants and bees.
+#
+# This example demonstrates how the default trainer class can be overridden
+# so that we can record classification metrics
+########################################################################
+
+import argparse
+import os
+from functools import partial
+
+from accelerate.utils import set_seed
+from torch import nn, optim
+from torch.optim import lr_scheduler
+from torchmetrics import ConfusionMatrix, Accuracy
+from torchvision import transforms, datasets, models
+
+from pytorch_accelerated.trainer import Trainer
+
+
+class TrainerWithMetrics(Trainer):
+    def __init__(self, num_classes, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cm_metrics = ConfusionMatrix(num_classes=num_classes)
+        self.accuracy = Accuracy(num_classes=num_classes)
+
+    def training_run_start(self):
+        self.cm_metrics.to(self._eval_dataloader.device)
+        self.accuracy.to(self._eval_dataloader.device)
+
+
+    def calculate_eval_batch_step(self, batch):
+        batch_output = super().calculate_eval_batch_step(batch)
+        preds = batch_output["model_outputs"].argmax(dim=-1)
+
+        self.cm_metrics.update(preds, batch[1])
+        self.accuracy.update(preds, batch[1])
+
+        return batch_output
+
+    def eval_epoch_end(self):
+        self.run_history.update_metric("confusion_matrix", self.cm_metrics.compute().cpu())
+        self.run_history.update_metric("accuracy", self.accuracy.compute().item())
+
+        self.cm_metrics.reset()
+        self.accuracy.reset()
+
+
+def main(data_dir):
+    set_seed(42)
+
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    data_transforms = {
+        "train": transforms.Compose(
+            [
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        ),
+        "val": transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        ),
+    }
+
+    # Create datasets
+    image_datasets = {
+        x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+        for x in ["train", "val"]
+    }
+    num_classes = len(image_datasets["train"].classes)
+
+    # Create a model
+    model = models.resnet18(pretrained=True)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+
+    # Define loss function
+    loss_func = nn.CrossEntropyLoss()
+
+    # Create optimizer and scheduler
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    exp_lr_scheduler = partial(lr_scheduler.StepLR, step_size=7, gamma=0.1)
+
+    trainer = TrainerWithMetrics(
+        num_classes=num_classes,
+        model=model,
+        loss_func=loss_func,
+        optimizer=optimizer,
+        scheduler_type=exp_lr_scheduler,
+    )
+
+    trainer.train(
+        train_dataset=image_datasets["train"],
+        eval_dataset=image_datasets["val"],
+        num_epochs=4,
+        per_device_batch_size=32,
+        fp16=True,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simple examples of training script.")
+    parser.add_argument("--data_dir", required=True, help="The data folder on disk.")
+    args = parser.parse_args()
+    main(args.data_dir)
---
+++ examples/vision/huggingface_pets_classicifation/hf_pets_cv_example.py
@@ -0,0 +1,212 @@
+########################################################################
+# This example trains a ResNet50 on the Oxford-IIT Pet Dataset and
+# was adapted from an example produced by HuggingFace available here:
+# https://github.com/huggingface/accelerate/blob/main/examples/cv_example.py
+#
+########################################################################
+
+import argparse
+import os
+import re
+from functools import partial
+
+import PIL
+import numpy as np
+import torch
+from timm import create_model
+from torch.optim.lr_scheduler import OneCycleLR
+from torch.utils.data import Dataset
+from torchvision.transforms import Compose, RandomResizedCrop, Resize, ToTensor
+
+from pytorch_accelerated.callbacks import (
+    TerminateOnNaNCallback,
+    PrintMetricsCallback,
+    PrintProgressCallback,
+    ProgressBarCallback,
+)
+from pytorch_accelerated.trainer import Trainer
+
+
+class PetsTrainer(Trainer):
+    def training_run_start(self):
+        self.mean = torch.tensor(self.model.default_cfg["mean"])[
+            None, :, None, None
+        ].to(self._accelerator.device)
+        self.std = torch.tensor(self.model.default_cfg["std"])[None, :, None, None].to(
+            self._accelerator.device
+        )
+
+    def calculate_train_batch_loss(self, batch):
+        inputs = (batch["image"] - self.mean) / self.std
+
+        return super(PetsTrainer, self).calculate_train_batch_loss(
+            (inputs, batch["label"])
+        )
+
+    def eval_epoch_start(self):
+        super(PetsTrainer, self).eval_epoch_start()
+        self.accurate = 0
+        self.num_elems = 0
+
+    def calculate_eval_batch_step(self, batch):
+        inputs = (batch["image"] - self.mean) / self.std
+
+        with torch.no_grad():
+            outputs = self.model(inputs)
+            loss = self.loss_func(outputs, batch["label"])
+        predictions = outputs.argmax(dim=-1)
+        accurate_preds = self._accelerator.gather(
+            predictions
+        ) == self._accelerator.gather(batch["label"])
+        self.num_elems += accurate_preds.shape[0]
+        self.accurate += accurate_preds.long().sum()
+
+        return {
+            "loss": loss,
+            "model_outputs": outputs,
+            "batch_size": inputs.size(0),
+        }
+
+    def eval_epoch_end(self):
+        super().eval_epoch_end()
+        self.run_history.update_metric(
+            "accuracy", round(100 * self.accurate.item() / self.num_elems, 2)
+        )
+
+    def create_scheduler(self, optimizer):
+        return self.scheduler_type(
+            optimizer=optimizer, steps_per_epoch=len(self._train_dataloader)
+        )
+
+
+def extract_label(fname):
+    # Function to get the label from the filename
+    stem = fname.split(os.path.sep)[-1]
+    return re.search(r"^(.*)_\d+\.jpg$", stem).groups()[0]
+
+
+class PetsDataset(Dataset):
+    def __init__(self, file_names, image_transform=None, label_to_id=None):
+        self.file_names = file_names
+        self.image_transform = image_transform
+        self.label_to_id = label_to_id
+
+    def __len__(self):
+        return len(self.file_names)
+
+    def __getitem__(self, idx):
+        fname = self.file_names[idx]
+        raw_image = PIL.Image.open(fname)
+        image = raw_image.convert("RGB")
+        if self.image_transform is not None:
+            image = self.image_transform(image)
+        label = extract_label(fname)
+        if self.label_to_id is not None:
+            label = self.label_to_id[label]
+        return {"image": image, "label": label}
+
+
+def training_function(data_dir, config):
+    lr = config["lr"]
+    num_epochs = int(config["num_epochs"])
+    seed = int(config["seed"])
+    batch_size = int(config["batch_size"])
+    image_size = config["image_size"]
+    if not isinstance(image_size, (list, tuple)):
+        image_size = (image_size, image_size)
+
+    # Grab all the image filenames
+    file_names = [
+        os.path.join(data_dir, fname)
+        for fname in os.listdir(data_dir)
+        if fname.endswith(".jpg")
+    ]
+
+    # Build the label correspondences
+    all_labels = [extract_label(fname) for fname in file_names]
+    id_to_label = list(set(all_labels))
+    id_to_label.sort()
+    label_to_id = {lbl: i for i, lbl in enumerate(id_to_label)}
+
+    # Set the seed before splitting the data.
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    # Split our filenames between train and validation
+    random_perm = np.random.permutation(len(file_names))
+    cut = int(0.8 * len(file_names))
+    train_split = random_perm[:cut]
+    eval_split = random_perm[cut:]
+
+    # For training we use a simple RandomResizedCrop
+    train_tfm = Compose([RandomResizedCrop(image_size, scale=(0.5, 1.0)), ToTensor()])
+    train_dataset = PetsDataset(
+        [file_names[i] for i in train_split],
+        image_transform=train_tfm,
+        label_to_id=label_to_id,
+    )
+
+    # For evaluation, we use a deterministic Resize
+    eval_tfm = Compose([Resize(image_size), ToTensor()])
+    eval_dataset = PetsDataset(
+        [file_names[i] for i in eval_split],
+        image_transform=eval_tfm,
+        label_to_id=label_to_id,
+    )
+
+    # Instantiate the model (we build the model here so that the seed also control new weights initialization)
+    model = create_model("resnet50d", pretrained=True, num_classes=len(label_to_id))
+
+    # Freezing the base model
+    for param in model.parameters():
+        param.requires_grad = False
+    for param in model.get_classifier().parameters():
+        param.requires_grad = True
+
+    # Define a loss function
+    loss_func = torch.nn.functional.cross_entropy
+
+    # Instantiate optimizer and scheduler type
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=lr / 25)
+    lr_scheduler = partial(
+        OneCycleLR,
+        max_lr=lr,
+        epochs=num_epochs,
+    )
+
+    trainer = PetsTrainer(
+        model=model,
+        loss_func=loss_func,
+        optimizer=optimizer,
+        scheduler_type=lr_scheduler,
+        callbacks=(
+            TerminateOnNaNCallback,
+            PrintProgressCallback,
+            ProgressBarCallback,
+            PrintMetricsCallback,
+        ),
+    )
+
+    trainer.train(
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        num_epochs=num_epochs,
+        per_device_batch_size=batch_size,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simple examples of training script.")
+    parser.add_argument("--data_dir", required=True, help="The data folder on disk.")
+    args = parser.parse_args()
+    # Sample hyper-parameters for learning rate, batch size, seed and a few other HPs
+    config = {
+        "lr": 3e-2,
+        "num_epochs": 3,
+        "seed": 42,
+        "batch_size": 64,
+        "image_size": 224,
+    }
+
+    training_function(args.data_dir, config)
