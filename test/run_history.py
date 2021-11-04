af28e5a2bb87c3832ec80b6f5a99850c0b120c80
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2021-11-04 12:11:45 +0000

    add run_history tests

--- test/run_history.py
+++ test/run_history.py
@@ -19,14 +19,52 @@ def test_can_increment_epoch(run_history: InMemoryRunHistory):
     assert incremented_once == initial_epoch_count +1
     assert incremented_twice == initial_epoch_count +2
 
-def test_can_update_metric():
-    pass
+def test_can_get_metric_values(run_history: InMemoryRunHistory):
+    metric_name = 'my_metric'
+    expected_metric_values = [0, 1, 2]
+    run_history._metrics[metric_name].extend(expected_metric_values)
 
-def test_can_get_metric_names():
-    pass
+    metric_values = run_history.get_metric(metric_name)
 
-def test_can_get_latest_metric():
-    pass
+    assert metric_values == expected_metric_values
 
-def test_can_reset():
-    pass
+def test_can_update_metric(run_history: InMemoryRunHistory):
+    metric_name = 'my_metric'
+    expected_metric_values = [0, 1, 2]
+
+    for value in expected_metric_values:
+        run_history.update_metric(metric_name, value)
+
+    assert run_history.get_metric(metric_name) == expected_metric_values
+
+def test_can_get_latest_metric(run_history: InMemoryRunHistory):
+    metric_name = 'my_metric'
+    metric_values = [0, 1, 2]
+    for value in metric_values:
+        run_history.update_metric(metric_name, value)
+
+    latest_metric = run_history.get_latest_metric(metric_name)
+
+    assert latest_metric == metric_values[-1]
+
+
+def test_can_get_metric_names(run_history: InMemoryRunHistory):
+    expected_metric_names = {'my_metric_1', 'my_metric_2'}
+    expected_metric_values = [0, 1]
+    for name, value in zip(expected_metric_names, expected_metric_values):
+        run_history.update_metric(name, value)
+
+    metric_names = run_history.get_metric_names()
+
+    assert metric_names == expected_metric_names
+
+
+def test_can_reset(run_history: InMemoryRunHistory):
+    metric_names = {'my_metric_1', 'my_metric_2'}
+    metric_values = [0, 1]
+    for name, value in zip(metric_names, metric_values):
+        run_history.update_metric(name, value)
+
+    run_history.reset()
+
+    assert len(run_history.get_metric_names()) == 0
