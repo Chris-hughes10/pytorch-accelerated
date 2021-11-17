3ec0238b2b9214b043e6bc90e56c13680756e291
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2021-11-17 14:17:37 +0000

    update config

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ release = "0.1.3"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 # autodoc_mock_imports = ["accelerate", "torch", 'numpy', 'tqdm']
 
 # Add any paths that contain templates here, relative to this directory.
