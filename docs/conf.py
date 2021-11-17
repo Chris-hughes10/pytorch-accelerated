0a186b08f813e41150b6b5521fbb2509559301f3
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2021-11-17 13:22:11 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-#
-# sys.path.insert(0, os.path.abspath("../pytorch_accelerated"))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(1, os.path.abspath("../pytorch_accelerated"))
 
 # -- Project information -----------------------------------------------------
 
@@ -32,7 +33,7 @@ release = "0.1.3"
 extensions = ["sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+# templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -49,4 +50,4 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
