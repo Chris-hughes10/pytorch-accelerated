e6c1eaa20c5fe7f95b644388f95e98f8c1f712c3
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2021-11-17 11:45:22 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Chris Hughes"
 author = "Chris Hughes"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.1"
+release = "0.2.2"
 
 
 # -- General configuration ---------------------------------------------------
