c8428d16e3687af237aea78e2eb47d0298310e7e
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2021-11-17 14:04:09 +0000

    try installing

--- docs/conf-working.py
+++ docs/conf-working.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-# sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
@@ -30,7 +30,7 @@ release = "0.1.3"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["sphinx.ext.autodoc"]
-# autodoc_mock_imports = ["accelerate", "torch", 'numpy', 'tqdm']
+autodoc_mock_imports = ["accelerate", "torch", 'numpy', 'tqdm']
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ["_templates"]
