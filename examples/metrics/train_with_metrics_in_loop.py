7b2b435d290117dd507e633a6e41b6becc342968
Chris Hughes <chris.hughes@microsoft.com>
2021-11-24 10:52:17 +0000

    Update examples

--- examples/metrics/train_with_metrics_in_loop.py
+++ examples/metrics/train_with_metrics_in_loop.py
@@ -1,10 +1,6 @@
-# Modifications Copyright © 2021 Chris Hughes
 ########################################################################
-# This is an accelerated example of the PyTorch "Transfer Learning for Computer Vision Tutorial"
-# written by Sasank Chilamkurthy, available here:
-# https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html,
-# which demonstrates fine-tuning a ResNet18 model to classify ants and bees.
-#
+# This example trains a model on the MNIST Dataset
+
 # This example demonstrates how the default trainer class can be overridden
 # so that we can record classification metrics
 #
@@ -12,15 +8,29 @@
 ########################################################################
 
 import os
-from functools import partial
 
-from accelerate import notebook_launcher
 from torch import nn, optim
-from torch.optim import lr_scheduler
-from torchmetrics import ConfusionMatrix, Accuracy, MetricCollection
-from torchvision import transforms, datasets, models
+from torch.utils.data import random_split
+from torchmetrics import MetricCollection, Accuracy, Precision, Recall
+from torchvision import transforms
+from torchvision.datasets import MNIST
 
-from pytorch_accelerated.trainer import Trainer
+from pytorch_accelerated import Trainer
+
+
+class MNISTModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.main = nn.Sequential(
+            nn.Linear(in_features=784, out_features=128),
+            nn.ReLU(),
+            nn.Linear(in_features=128, out_features=64),
+            nn.ReLU(),
+            nn.Linear(in_features=64, out_features=10),
+        )
+
+    def forward(self, x):
+        return self.main(x.view(x.shape[0], -1))
 
 
 class TrainerWithMetrics(Trainer):
@@ -32,7 +42,8 @@ class TrainerWithMetrics(Trainer):
         self.metrics = MetricCollection(
             {
                 "accuracy": Accuracy(num_classes=num_classes),
-                "confusion_matrix": ConfusionMatrix(num_classes=num_classes),
+                "precision": Precision(num_classes=num_classes),
+                "recall": Recall(num_classes=num_classes),
             }
         )
 
@@ -47,75 +58,39 @@ class TrainerWithMetrics(Trainer):
     def eval_epoch_end(self):
         metrics = self.metrics.compute()
         self.run_history.update_metric("accuracy", metrics["accuracy"].cpu())
-        self.run_history.update_metric(
-            "confusion matrix", metrics["confusion_matrix"].cpu()
-        )
+        self.run_history.update_metric("precision", metrics["precision"].cpu())
+        self.run_history.update_metric("recall", metrics["recall"].cpu())
 
         self.metrics.reset()
 
 
-# def main(data_dir):
 def main():
-    data_dir = "/home/chris/notebooks/hymenoptera_data/"
+    dataset = MNIST(os.getcwd(), download=True, transform=transforms.ToTensor())
+    num_classes = len(dataset.class_to_idx)
 
-    # Data augmentation and normalization for training
-    # Just normalization for validation
-    data_transforms = {
-        "train": transforms.Compose(
-            [
-                transforms.RandomResizedCrop(224),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-            ]
-        ),
-        "val": transforms.Compose(
-            [
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-            ]
-        ),
-    }
-
-    # Create datasets
-    image_datasets = {
-        x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
-        for x in ["train", "val"]
-    }
-    num_classes = len(image_datasets["train"].classes)
-
-    # Create a model
-    model = models.resnet18(pretrained=True)
-    model.fc = nn.Linear(model.fc.in_features, num_classes)
-
-    # Define loss function
+    train_dataset, validation_dataset, test_dataset = random_split(
+        dataset, [50000, 5000, 5000]
+    )
+    model = MNISTModel()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     loss_func = nn.CrossEntropyLoss()
 
-    # Create optimizer and scheduler
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    exp_lr_scheduler = partial(lr_scheduler.StepLR, step_size=7, gamma=0.1)
-
     trainer = TrainerWithMetrics(
-        num_classes=num_classes,
-        model=model,
-        loss_func=loss_func,
-        optimizer=optimizer,
+        model=model, loss_func=loss_func, optimizer=optimizer, num_classes=num_classes
     )
 
     trainer.train(
-        train_dataset=image_datasets["train"],
-        eval_dataset=image_datasets["val"],
-        num_epochs=4,
+        train_dataset=train_dataset,
+        eval_dataset=validation_dataset,
+        num_epochs=2,
         per_device_batch_size=32,
-        create_scheduler_fn=exp_lr_scheduler,
+    )
+
+    trainer.evaluate(
+        dataset=test_dataset,
+        per_device_batch_size=64,
     )
 
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description="Simple examples of training script.")
-    # parser.add_argument("--data_dir", required=True, help="The data folder on disk.")
-    # args = parser.parse_args()
-    # main(args.data_dir)
-    notebook_launcher(main, num_processes=2)
+    main()
