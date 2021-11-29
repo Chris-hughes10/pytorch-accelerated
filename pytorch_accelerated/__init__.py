365306eb4d968533fff615c4bf8948a1187fea6d
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2021-11-29 13:02:31 +0000

    Add import wrapper for notebook launcher

--- pytorch_accelerated/__init__.py
+++ pytorch_accelerated/__init__.py
@@ -1,6 +1,7 @@
 import logging
 from . import _version
 
+from accelerate import notebook_launcher as accelerate_notebook_launcher
 from accelerate.utils import set_seed
 from pytorch_accelerated.trainer import Trainer, TrainerPlaceholderValues
 
@@ -9,5 +10,6 @@ logger.setLevel(logging.INFO)
 
 logger.info("Setting random seeds")
 set_seed(42)
+notebook_launcher = accelerate_notebook_launcher
 
 __version__ = _version.get_versions()["version"]
