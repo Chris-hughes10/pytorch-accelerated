83dc54bf3e5cbda4fbc7ef8ed2f7fa2aad01a5aa
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2022-04-16 13:02:20 +0100

    update docstrings and tests

--- pytorch_accelerated/schedulers/cosine_scheduler.py
+++ pytorch_accelerated/schedulers/cosine_scheduler.py
@@ -2,6 +2,7 @@
 
 import math
 from functools import partial
+from typing import Callable
 
 import torch
 
@@ -11,10 +12,19 @@ from pytorch_accelerated.schedulers.scheduler_base import StatefulSchedulerBase
 
 class CosineLrScheduler(StatefulSchedulerBase):
     """
-    Cosine decay. without restarts
-    This is described in the paper https://arxiv.org/abs/1608.03983.
+    Cosine annealing learning rate scheduler, as described in this paper https://arxiv.org/abs/1608.03983,
+    but without restarts.
 
-    k-decay option based on `k-decay: A New Method For Learning Rate Schedule` - https://arxiv.org/abs/2004.05909, explain
+    This scheduler differs from the PyTorch's ``~torch.optim.lr_scheduler.CosineAnnealingLR`` as it provides options
+    to add warmup and cooldown epochs. Additionally, the annealing rate can be modified by adjusting the k-decay
+    parameter, for which the rate of change of the learning rate is changed by its k-th order derivative,
+    as described in this paper https://arxiv.org/abs/2004.05909.
+
+    If warmup epochs are specified, the learning rate will increase in constant increments from the `warmup_starting_lr`
+    provided until the learning rate specified in the parameter group is reached.
+
+    If cooldown epochs are specified, the learning rate will be fixed at the minimum lr value given. This behaviour will
+    continue if the scheduler is called after the training cycle has completed.
     """
 
     def __init__(
@@ -23,23 +33,31 @@ class CosineLrScheduler(StatefulSchedulerBase):
         total_num_epochs: int,
         num_update_steps_per_epoch: int,
         k_decay=1.0,
-        lr_min: float = 0.0,
+        lr_min: float = 1e-6,
         min_lr_ratio=None,
         num_warmup_epochs: int = 0,
-        warmup_starting_lr=0,
+        warmup_starting_lr=1e-6,
+        warmup_starting_lr_ratio=None,
         num_cooldown_epochs=0,
     ):
         """
+        Create a new ConsineLrScheduler object which can be used to modify the learning rate in an optimizer's parameter
+        groups.
 
-        :param optimizer:
-        :param total_num_epochs:
-        :param num_update_steps_per_epoch:
-        :param k_decay:
-        :param lr_min:
-        :param min_lr_ratio:
-        :param num_warmup_epochs:
-        :param warmup_starting_lr:
-        :param num_cooldown_epochs:
+        :param optimizer: a PyTorch optimizer containing one or more parameter groups
+        :param total_num_epochs: the total number of training epochs, inclusive of any warmup and cooldown epochs
+        :param num_update_steps_per_epoch: the number of optimizer updates that take place per epoch
+        :param k_decay: adjusts the rate of annealing. Higher values will maintain a higher lr for longer
+        :param lr_min: the minimum value that the learning rate should decay to for all parameter groups.
+        This will be held fixed during cooldown epochs
+        :param min_lr_ratio: this can be used to represent the minimum lr for each parameter group as a ratio of its maximum lr.
+         If set, this will take precedence over `lr_min`
+        :param num_warmup_epochs: the number of epochs to gradually increase the lr until it reaches the maximum value
+        :param warmup_starting_lr: the starting lr that will be used for all parameter groups at the beginning of training
+        if `num_warmup_epochs` is greater than 0
+        :param warmup_starting_lr_ratio: this can be used to represent the warmup starting lr for each parameter group as a
+        ratio of its maximum lr. If set, this will take precedence over `warmup_starting_lr`
+        :param num_cooldown_epochs: the number of epochs to hold the lr at its minimum value
         """
 
         super().__init__(optimizer)
@@ -50,21 +68,36 @@ class CosineLrScheduler(StatefulSchedulerBase):
         self.lr_min = lr_min
         self.warmup_iterations = num_warmup_epochs * num_update_steps_per_epoch
         self.warmup_lr_init = warmup_starting_lr
+        self.warmup_lr_ratio = warmup_starting_lr_ratio
         self.k_decay = k_decay
         self.num_cooldown_iterations = num_cooldown_epochs * num_update_steps_per_epoch
         if self.warmup_iterations:
             self._update_param_groups(self.warmup_lr_init)
 
     def get_updated_values(self, num_updates: int):
+        """
+        Calculate the learning rate for a particular step given the number of previous updates.
+
+        If warmup epochs are specified, the learning rate will increase in constant increments from the `warmup_starting_lr`
+        provided until the learning rate specified in the parameter group is reached.
+
+        If cooldown epochs are specified, the learning rate will be fixed at the minimum lr value given. This behaviour will
+        continue if the scheduler is called after the training cycle has completed.
+
+        Between any warmup or cooldown epochs, the cosine annealing strategy will be used.
+
+        :param num_updates: the number of previous updates
+        :return: the learning rates with which to update each parameter group
+        """
         if num_updates < self.warmup_iterations:
             # increase lr linearly
             lrs = [
-                self.warmup_lr_init
+                (self.warmup_lr_ratio * lr if self.warmup_lr_ratio is not None else self.warmup_lr_init)
                 + num_updates * ((lr - self.warmup_lr_init) / self.warmup_iterations)
                 for lr in self.base_lr_values
             ]
-        elif (num_updates - self.warmup_iterations) < (
-            self.total_iterations - self.num_cooldown_iterations
+        elif num_updates < (
+            self.total_iterations - (self.warmup_iterations + self.num_cooldown_iterations)
         ):
             num_updates = num_updates - self.warmup_iterations
             total_cosine_iterations = self.total_iterations - (
@@ -109,16 +142,48 @@ class CosineLrScheduler(StatefulSchedulerBase):
         return lrs
 
     @classmethod
-    def create_scheduler(
+    def create_scheduler_fn(
         cls,
         total_num_epochs: int = TrainerPlaceholderValues.NUM_EPOCHS,
         num_iterations_per_epoch: int = TrainerPlaceholderValues.NUM_UPDATE_STEPS_PER_EPOCH,
         k_decay=1.0,
-        lr_min: float = 0.0,
+        lr_min: float = 1e-6,
         min_lr_ratio=None,
-        warmup_lr_init=0,
         num_warmup_epochs: int = 0,
-    ):
+        warmup_starting_lr=1e-6,
+        warmup_starting_lr_ratio=None,
+        num_cooldown_epochs=0,
+    ) -> Callable:
+        """
+        An alternative constructor which returns a function that accepts an optimizer and creates an instance of
+        `CosineLrScheduler`. This is primarily intended to be used with the :class:`~pytorch_accelerated.trainer.Trainer`
+        as illustrated below:
+
+            trainer.train(
+            train_dataset=train_dataset,
+            num_epochs=num_epochs,
+            per_device_batch_size=batch_size,
+            create_scheduler_fn=CosineLrScheduler.create_scheduler_fn(num_warmup_epochs=5),
+            )
+
+        By default, the `total_num_epochs` and `num_iterations_per_epoch` arguments will be set by the
+        :class:`~pytorch_accelerated.trainer.Trainer` with the correct values at runtime.
+
+        :param total_num_epochs: the total number of training epochs, inclusive of any warmup and cooldown epochs
+        :param num_update_steps_per_epoch: the number of optimizer updates that take place per epoch
+        :param k_decay: adjusts the rate of annealing. Higher values will maintain a higher lr for longer
+        :param lr_min: the minimum value that the learning rate should decay to for all parameter groups.
+        This will be held fixed during cooldown epochs
+        :param min_lr_ratio: this can be used to represent the minimum lr for each parameter group as a ratio of its maximum lr.
+         If set, this will take precedence over `lr_min`
+        :param num_warmup_epochs: the number of epochs to gradually increase the lr until it reaches the maximum value
+        :param warmup_starting_lr: the starting lr that will be used for all parameter groups at the beginning of training
+        if `num_warmup_epochs` is greater than 0
+        :param warmup_starting_lr_ratio: this can be used to represent the warmup starting lr for each parameter group as a
+        ratio of its maximum lr. If set, this will take precedence over `warmup_starting_lr`
+        :param num_cooldown_epochs: the number of epochs to hold the lr at its minimum value
+        :return: a function which accepts an optimizer as an argument and returns an instance of `CosineLrScheduler`
+        """
         return partial(
             cls,
             total_num_epochs=total_num_epochs,
@@ -127,5 +192,7 @@ class CosineLrScheduler(StatefulSchedulerBase):
             min_lr_ratio=min_lr_ratio,
             k_decay=k_decay,
             num_warmup_epochs=num_warmup_epochs,
-            warmup_lr_init=warmup_lr_init,
+            warmup_starting_lr=warmup_starting_lr,
+            warmup_starting_lr_ratio=warmup_starting_lr_ratio,
+            num_cooldown_epochs=num_cooldown_epochs
         )
--- test/scheduler/test_cosine_scheduler.py
+++ test/scheduler/test_cosine_scheduler.py
@@ -105,7 +105,10 @@ def test_cooldown_epochs_at_lr_min():
     lr_1_max = 0.01
     lr_2_max = 0.002
     lr_min = 1e-6
+    expected_cooldown_steps = [lr_min] * num_cooldown_steps
     model, optimizer = create_model_and_optimizer(lr_1_max, lr_2_max)
+
+
     scheduler = CosineLrScheduler(optimizer,
                                   total_num_epochs=num_epochs,
                                   num_update_steps_per_epoch=num_steps_per_epoch,
@@ -116,13 +119,89 @@ def test_cooldown_epochs_at_lr_min():
                                                          num_epochs,
                                                          num_steps_per_epoch)
 
-    assert group_1_lrs[-num_cooldown_steps:] == [lr_min] * num_cooldown_steps
-    assert group_2_lrs[-num_cooldown_steps:] == [lr_min] * num_cooldown_steps
+    assert group_1_lrs[-num_cooldown_steps:] == expected_cooldown_steps
+    assert group_2_lrs[-num_cooldown_steps:] == expected_cooldown_steps
 
 
 def test_warmup():
-    pass
+    num_epochs = 10
+    num_warmup_epochs = 2
+    num_steps_per_epoch = 10
+    num_warmup_steps = num_warmup_epochs * num_steps_per_epoch
+    lr_1_max = 0.01
+    lr_2_max = 0.002
+    warmup_lr = 1e-6
+    model, optimizer = create_model_and_optimizer(lr_1_max, lr_2_max)
+    scheduler = CosineLrScheduler(optimizer,
+                                  total_num_epochs=num_epochs,
+                                  num_update_steps_per_epoch=num_steps_per_epoch,
+                                  num_warmup_epochs=num_warmup_epochs,
+                                  warmup_starting_lr=warmup_lr)
+
+    group_1_lrs, group_2_lrs = collect_lrs_for_scheduler(scheduler,
+                                                         num_epochs,
+                                                         num_steps_per_epoch)
+
+    assert group_1_lrs[0] == warmup_lr
+    assert group_2_lrs[0] == warmup_lr
+    assert group_1_lrs[num_warmup_steps] == max(group_1_lrs)
+    assert group_2_lrs[num_warmup_steps] == max(group_2_lrs)
+
+def test_warmup_from_ratio():
+    num_epochs = 10
+    num_warmup_epochs = 2
+    num_steps_per_epoch = 10
+    num_warmup_steps = num_warmup_epochs * num_steps_per_epoch
+    lr_1_max = 0.01
+    lr_2_max = 0.002
+    warmup_lr_ratio = 0.01
+    starting_lr_1 = warmup_lr_ratio * lr_1_max
+    starting_lr_2 = warmup_lr_ratio * lr_2_max
+
+    model, optimizer = create_model_and_optimizer(lr_1_max, lr_2_max)
+    scheduler = CosineLrScheduler(optimizer,
+                                  total_num_epochs=num_epochs,
+                                  num_update_steps_per_epoch=num_steps_per_epoch,
+                                  num_warmup_epochs=num_warmup_epochs,
+                                  warmup_starting_lr_ratio=warmup_lr_ratio)
+
+    group_1_lrs, group_2_lrs = collect_lrs_for_scheduler(scheduler,
+                                                         num_epochs,
+                                                         num_steps_per_epoch)
+
+    assert group_1_lrs[0] == starting_lr_1
+    assert group_2_lrs[0] == starting_lr_2
+    assert group_1_lrs[num_warmup_steps] == max(group_1_lrs)
+    assert group_2_lrs[num_warmup_steps] == max(group_2_lrs)
 
 
 def test_warmup_and_cooldown():
-    pass
+    num_epochs = 10
+    num_warmup_epochs = 2
+    num_cooldown_epochs = 2
+    num_steps_per_epoch = 10
+    num_warmup_steps = num_warmup_epochs * num_steps_per_epoch
+    num_cooldown_steps = num_cooldown_epochs * num_steps_per_epoch
+    lr_1_max = 0.01
+    lr_2_max = 0.002
+    lr_min = 1e-6
+    expected_cooldown_steps = [lr_min] * num_cooldown_steps
+    model, optimizer = create_model_and_optimizer(lr_1_max, lr_2_max)
+    scheduler = CosineLrScheduler(optimizer,
+                                  total_num_epochs=num_epochs,
+                                  num_update_steps_per_epoch=num_steps_per_epoch,
+                                  num_warmup_epochs=num_warmup_epochs,
+                                  warmup_starting_lr=lr_min,
+                                  lr_min=lr_min,
+                                  num_cooldown_epochs=2)
+
+    group_1_lrs, group_2_lrs = collect_lrs_for_scheduler(scheduler,
+                                                         num_epochs,
+                                                         num_steps_per_epoch)
+
+    assert group_1_lrs[0] == lr_min
+    assert group_2_lrs[0] == lr_min
+    assert group_1_lrs[num_warmup_steps] == max(group_1_lrs)
+    assert group_2_lrs[num_warmup_steps] == max(group_2_lrs)
+    assert group_1_lrs[-num_cooldown_steps:] == expected_cooldown_steps
+    assert group_2_lrs[-num_cooldown_steps:] == expected_cooldown_steps
