74b4a7d9e986c21296865055761f1d1ff604f278
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2022-04-28 06:58:28 +0100

    Update accelerate version (#26)
    * Update requirements.txt
    
    * update setting seeds with new accelerate version

--- pytorch_accelerated/__init__.py
+++ pytorch_accelerated/__init__.py
@@ -1,4 +1,7 @@
 import logging
+
+from accelerate.state import AcceleratorState
+
 from . import _version
 
 from accelerate import notebook_launcher as accelerate_notebook_launcher
@@ -7,9 +10,13 @@ from pytorch_accelerated.trainer import Trainer, TrainerPlaceholderValues
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO)
+
+# initialise singleton
+state = AcceleratorState(_from_accelerator=True)
 
 logger.info("Setting random seeds")
-set_seed(42)
+set_seed(42, device_specific=True)
 notebook_launcher = accelerate_notebook_launcher
 
 __version__ = _version.get_versions()["version"]
