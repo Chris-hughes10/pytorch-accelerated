c9c6c37dda66ca64634e2e375d1b85cdf00f4c9d
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2022-09-23 14:23:53 +0100

    Update utils.py

--- test/utils.py
+++ test/utils.py
@@ -1,8 +1,11 @@
 from unittest.mock import Mock
 
+import torch
 from pytorch_accelerated import Trainer
 from pytorch_accelerated.callbacks import LimitBatchesCallback
-from pytorch_accelerated.utils import LimitBatches
+from pytorch_accelerated.utils import LimitBatches, remove_padding
+
+PAD_VALUE = -1
 
 
 def test_can_limit_batches_with_manager():
@@ -24,3 +27,34 @@ def test_limit_batches_not_present_outside_manager():
     assert LimitBatchesCallback not in {
         callback.__class__ for callback in trainer.callback_handler.callbacks
     }
+
+
+def test_can_remove_padding_1d():
+    t = torch.tensor([0, 1, 2, 3, PAD_VALUE, PAD_VALUE])
+    expected_t = torch.tensor([0, 1, 2, 3])
+
+    actual_t = remove_padding(t, PAD_VALUE)
+
+    assert torch.eq(expected_t, actual_t).all()
+
+
+def test_can_remove_padding_2d():
+    t = torch.tensor([[0, 1], [1, 2], [PAD_VALUE, PAD_VALUE], [PAD_VALUE, PAD_VALUE]])
+
+    expected_t = torch.tensor([[0, 1], [1, 2]])
+
+    actual_t = remove_padding(t, PAD_VALUE)
+
+    assert torch.eq(expected_t, actual_t).all()
+
+
+def test_can_remove_padding_3d():
+    t = torch.tensor(
+        [[[0, 1], [1, 2]], [[PAD_VALUE, PAD_VALUE], [PAD_VALUE, PAD_VALUE]]]
+    )
+
+    expected_t = torch.tensor([[[0, 1], [1, 2]]])
+
+    actual_t = remove_padding(t, PAD_VALUE)
+
+    assert torch.eq(expected_t, actual_t).all()
