622dafb9c291e11489e652796e6800ff673f1564
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2023-01-04 10:38:35 +0000

    add tests for loss tracker

---
+++ test/test_loss_tracking.py
@@ -0,0 +1,33 @@
+
+import torch
+from pytorch_accelerated.tracking import LossTracker
+
+
+def test_can_track_loss_incrementally():
+    tracker = LossTracker()
+    losses = [105.5, 94.7, 98., 55.2, 32.5]
+    expected_loss = sum(losses)/len(losses)
+
+    for loss in losses:
+        tracker.update(loss)
+    calculated_loss = tracker.average
+
+    assert expected_loss == calculated_loss
+
+def test_can_track_loss_for_varying_batch_sizes():
+    tracker = LossTracker()
+    losses = [105.5, 94.7, 98., 55.2, 32.5]
+    batch_sizes = [64, 32, 16, 8, 4]
+    total_count = sum(batch_sizes)
+    total_loss = (torch.tensor(losses) * torch.tensor(batch_sizes)).sum().item()
+    expected_loss = total_loss/total_count
+
+    for loss, batch_size in zip(losses, batch_sizes):
+        tracker.update(loss, batch_size=batch_size)
+    calculated_loss = tracker.average
+
+    assert expected_loss == calculated_loss
+
+
+
+
